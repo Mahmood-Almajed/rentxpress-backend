4250dc709cddec2a57ee39e08746f25d11bca32a
Mahmood-Almajed <mahmoodalmajed46@gmail.com>
2025-04-13 17:06:47 +0300

    chatbot fix

--- controllers/chatbot.js
+++ controllers/chatbot.js
@@ -18,7 +18,6 @@ Important:
 - Highlight ♿ for special needs compatible cars
 - Always return clickable links using markdown [Click here](url)
 - for the Mileage use "km" as the unit and separate thousands with a comma
-- if the user wrote in arabic or any other language, respond in the same language
 `;
 
 const functions = [
